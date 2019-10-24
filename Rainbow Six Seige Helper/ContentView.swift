76ba3519dec186c26dd81578bbb96154fd35aa0e
Matt Ginelli <matt_uhs@me.com>
2019-10-23 17:48:01 -1000

    Initial Commit

---
+++ Rainbow Six Seige Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rainbow Six Seige Helper
+//
+//  Created by Matthew Ginelli on 10/23/19.
+//  Copyright © 2019 MGinelli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
